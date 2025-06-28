89c869f98dd9574b0b19e472272bd4810f3a062c
ploMP4 <artopouloskon@gmail.com>
2025-06-28 14:55:51 +0300

    batman

---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use wayland_client::{
+    globals::{registry_queue_init, GlobalListContents},
+    protocol::{wl_pointer::ButtonState, wl_registry},
+    Connection, Dispatch, QueueHandle,
+};
+use wayland_protocols_wlr::virtual_pointer::v1::client::{
+    zwlr_virtual_pointer_manager_v1, zwlr_virtual_pointer_v1,
+};
+
+struct State;
+
+impl Dispatch<wl_registry::WlRegistry, GlobalListContents> for State {
+    fn event(
+        _state: &mut Self,
+        _: &wl_registry::WlRegistry,
+        _: wl_registry::Event,
+        _: &GlobalListContents,
+        _: &Connection,
+        _: &QueueHandle<State>,
+    ) {
+    }
+}
+
+impl Dispatch<zwlr_virtual_pointer_manager_v1::ZwlrVirtualPointerManagerV1, ()> for State {
+    fn event(
+        _state: &mut Self,
+        _: &zwlr_virtual_pointer_manager_v1::ZwlrVirtualPointerManagerV1,
+        _: zwlr_virtual_pointer_manager_v1::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<State>,
+    ) {
+    }
+}
+
+impl Dispatch<zwlr_virtual_pointer_v1::ZwlrVirtualPointerV1, ()> for State {
+    fn event(
+        _state: &mut Self,
+        _: &zwlr_virtual_pointer_v1::ZwlrVirtualPointerV1,
+        _: zwlr_virtual_pointer_v1::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<State>,
+    ) {
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let conn = Connection::connect_to_env()?;
+    let (globals, mut event_queue) = registry_queue_init::<State>(&conn)?;
+    let qh = event_queue.handle();
+
+    let manager: zwlr_virtual_pointer_manager_v1::ZwlrVirtualPointerManagerV1 =
+        globals.bind(&qh, 1..=2, ())?;
+
+    let v_pointer = manager.create_virtual_pointer(None, &qh, ());
+
+    v_pointer.motion_absolute(timestamp(), 100, 100, 1920, 1080);
+    v_pointer.frame();
+
+    conn.flush()?;
+
+    event_queue.roundtrip(&mut State)?;
+
+    v_pointer.destroy();
+    manager.destroy();
+
+    Ok(())
+}
+
+fn timestamp() -> u32 {
+    return SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as u32;
+}
